ff97b601bbdbfc1d76d50c52a604fd2a0b71ffc2
Andrew Montes <andrewmontes87@gmail.com>
2013-08-28 21:01:36 -0700

    range commit

--- primes.py
+++ primes.py
@@ -3,19 +3,19 @@
 
 numbers = {}
 primes = []
-limit = 1000
+limit = 10000
 
 print "PRINTING PRIMES UP TO LIMIT: %i" % limit
 
-for x in range(limit+1):
+for x in range(2, limit+1):
 	numbers[x] = 1
 
-for x in range(limit+1):
-	for y in range(limit+1-x):
-		if x > 1 and y > 1 and (x*y<limit+1):
+for x in range(2, limit+1):
+	for y in range(2, limit+1-x):
+		if x*y<limit+1:
 			numbers[x*y] = 0   # not prime
 
-for x in range(limit+1):
+for x in range(2, limit+1):
 	if numbers[x] == 1:
 		primes.append(x)
 
